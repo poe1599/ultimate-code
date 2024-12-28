f7fd760df216b91747c352e3333f2b9b00c40148
Allen Yeh <allen_yeh@systemweb.com.tw>
2024-12-28 21:57:13 +0800

    ✨feature: install primevue

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config';
+import preset from '~me/preset';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue, {
+  theme: {
+      preset: preset
+  }
+});
+
+app.mount('#app')
---
+++ src/preset.ts
@@ -0,0 +1,21 @@
+
+import { definePreset } from '@primevue/themes';
+import Aura from '@primevue/themes/aura';
+
+export default definePreset(Aura, {
+  semantic: {
+      primary: {
+          50: '{purple.50}',
+          100: '{purple.100}',
+          200: '{purple.200}',
+          300: '{purple.300}',
+          400: '{purple.400}',
+          500: '{purple.500}',
+          600: '{purple.600}',
+          700: '{purple.700}',
+          800: '{purple.800}',
+          900: '{purple.900}',
+          950: '{purple.950}'
+      }
+  }
+});
